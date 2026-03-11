dcda5279eda8a047a0b3e4606e0a542c96c8d1d9
SturdyPose <oleg.musijenko@seznam.cz>
2026-03-12 00:11:41 +0100

    Fix js build by adding stub

---
+++ src/alcotest-engine/stack_error_reporter.js
@@ -0,0 +1,7 @@
+//Provides: caml_setup_stub_exception_handler
+function caml_setup_stub_exception_handler(unit)
+{
+    // Intentionally left empty
+    // This function is mainly for C stubs and Segfault errors
+    return;
+}
