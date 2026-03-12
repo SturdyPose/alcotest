86c30479678704188720a25351efaae6697aa75d
SturdyPose <oleg.musijenko@seznam.cz>
2026-03-12 21:50:36 +0100

    Try to trigger segfault on mac

--- test/e2e/alcotest/stubs/nullexceptionstub.c
+++ test/e2e/alcotest/stubs/nullexceptionstub.c
@@ -1,12 +1,17 @@
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 
-#include <stdio.h>
+#if !defined(_WIN32)
+#include <signal.h>
+#endif
 
 value caml_segfault_call(void) {
   CAMLparam0();
-  int *someVal = NULL;
-  int a = *someVal;
-  printf("%i\n", a); // printf to force Apple plat evalation
-  CAMLreturn(Int_val(a));
+  volatile int *p = (volatile int *)0;
+  *p = 0xDEADBEEF;
+  #if !defined(_WIN32)
+  // in case mac won't call segfault
+  raise(SIGSEGV);
+  #endif
+  CAMLreturn(Int_val(0));
 }
